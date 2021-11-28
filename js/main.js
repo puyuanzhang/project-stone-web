290fc949e875dfa22c15b87543a7373989ec190d
zpyqaq <2433208256@qq.com>
2021-11-28 22:55:06 +0800

    create stone

--- js/main.js
+++ js/main.js
@@ -1,22 +1,38 @@
 let img; // 声明变量 'img'
+let stone1;
+let stone2;
+class stone{
+    constructor(img,x,y,dim,speed){
+        this.img = img;
+        this.x = x;
+        this.y = random(height);
+        this.dim = dim;
+        this.speed = speed;
+    }
+    put(){
+        image(this.img, this.x, this.y, this.img.width / 3, this.img.height / 3);
+    }
+    update(){
+        this.x = this.x + this.speed
+        if (this.x > this.width + this.dim) {
+            this.x = -this.dim;
+        }
+    }
+}
 
 function setup() {
     createCanvas(1080, 720);
-    img = loadImage('res/women.png'); // 加载图像
-}
-
-let x = 0
-let y = 0
-let speed = 1
-function update(){
-    x = x + speed;
+    img = loadImage('res/women.png');
+    stone1 = new stone(img,0,0,80,3);
+    stone2 = new stone(img,0,0,70,3);
 }
 
 function draw() {
     // 在坐标(0, 0)，显示原图大小的图像 
     background(0);
-    image(img, x, y, img.width / 3, img.height / 3);
-    
+    stone1.put();
+    stone1.update();
+    stone2.put();
+    stone2.update();
    
-    
 }
